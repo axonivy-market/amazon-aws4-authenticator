3533ce7fcd370f75725bd983a006a17afc0b31a2
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-09-11 08:55:29 +0200

    log requests; going over the wire

--- src/test/java/com/axonivy/connector/aws/authenticator/TestAws4AuthenticatorFeature.java
+++ src/test/java/com/axonivy/connector/aws/authenticator/TestAws4AuthenticatorFeature.java
@@ -2,6 +2,10 @@ package com.axonivy.connector.aws.authenticator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.glassfish.jersey.logging.LoggingFeature;
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.connector.aws.authentication.Aws4AuthenticationFeature;
@@ -13,10 +17,18 @@ import jakarta.ws.rs.core.Response.Status;
 
 class TestAws4AuthenticatorFeature {
 
+  private static final Logger LOGGER = Logger.getLogger(TestAws4AuthenticatorFeature.class.getName());
+  private static final LoggingFeature LOGGING = new LoggingFeature(
+      LOGGER,
+      Level.INFO,
+      LoggingFeature.Verbosity.PAYLOAD_ANY,
+      8192);
+
   @Test
   void authenticator_with_regionName() {
 
     var client = ClientBuilder.newBuilder()
+        .register(LOGGING)
         .register(Aws4AuthenticationFeature.class)
         .property("accessKey", getAccessKey())
         .property("secretKey", getSecretKey())
@@ -41,6 +53,7 @@ class TestAws4AuthenticatorFeature {
   void authenticator_without_regionName() {
 
     var client = ClientBuilder.newBuilder()
+        .register(LOGGING)
         .register(Aws4AuthenticationFeature.class)
         .property("accessKey", getAccessKey())
         .property("secretKey", getSecretKey())
@@ -61,17 +74,17 @@ class TestAws4AuthenticatorFeature {
   }
 
   private Object getSecretKey() {
-    var value = System.getProperty("aws.secret.key");
+    var value = System.getenv("AWS_SECRET_KEY");
     assertThat(value)
-        .as("AWS secret key must be provided using system property 'aws.secret.key'")
+        .as("AWS secret key must be provided using system property 'AWS_SECRET_KEY'")
         .isNotBlank();
     return value;
   }
 
   private Object getAccessKey() {
-    var value = System.getProperty("aws.access.key");
+    var value = System.getenv("AWS_ACCESS_KEY");
     assertThat(value)
-        .as("AWS access key must be provided using system property 'aws.access.key'")
+        .as("AWS access key must be provided using system property 'AWS_ACCESS_KEY'")
         .isNotBlank();
     return value;
   }
